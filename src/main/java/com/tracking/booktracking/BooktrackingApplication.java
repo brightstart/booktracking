14bb098cf6eb40bff9ef7ce916e4ebe536e708cd
Prateek Arora <aroraprateek091@outlook.com>
2024-05-23 22:25:29 +0530

    save books. Some of the books still not got stored. Need to check why.

--- src/main/java/com/tracking/booktracking/BooktrackingApplication.java
+++ src/main/java/com/tracking/booktracking/BooktrackingApplication.java
@@ -4,6 +4,7 @@ import com.tracking.booktracking.connect.DataStaxAstraProperties;
 import com.tracking.booktracking.model.Author;
 import com.tracking.booktracking.model.Book;
 import com.tracking.booktracking.repo.AuthorRepository;
+import com.tracking.booktracking.repo.BookRepository;
 import jakarta.annotation.PostConstruct;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -14,12 +15,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.format.datetime.DateFormatter;
 
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,11 +34,13 @@ public class BooktrackingApplication {
 
 	@Autowired AuthorRepository authorRepository;
 
-	@Value("${datadump.location.author}")
-	private String authorDumpLocation;
+	@Autowired BookRepository bookRepository;
 
-	@Value("${datadump.location.works}")
-	private String worksDumpLocation;
+//	@Value("${datadump.location.author}")
+//	private String authorDumpLocation;
+//
+//	@Value("${datadump.location.works}")
+//	private String worksDumpLocation;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooktrackingApplication.class, args);
@@ -56,6 +61,7 @@ public class BooktrackingApplication {
 	}
 
 	private void initAuthors() {
+		String authorDumpLocation = "C:/Users/arora/Downloads/test-authors.txt";
 		System.out.println(authorDumpLocation);
 		Path path = Paths.get(authorDumpLocation);
 		try(Stream<String> lines = Files.lines(path)){
@@ -78,64 +84,66 @@ public class BooktrackingApplication {
 		}
 	}
 	private void initWorks() {
-		System.out.println(authorDumpLocation);
-		Path path = Paths.get(authorDumpLocation);
-		try(Stream<String> lines = Files.lines(path)){
-			lines.forEach( line ->
-					{
-						String jsonString = line.substring(line.indexOf('{'));
+		String worksDumpLocation = "C:/Users/arora/Downloads/test-works.txt";
+		Path path = Paths.get(worksDumpLocation);
+		try(Stream<String> lines =Files.lines(path)){
+			lines.forEach(line->{
 						try {
-							JSONObject worksJsonObject = new JSONObject(jsonString);
+
+							String jsonString = line.substring(line.indexOf("{"));
+							JSONObject json = new JSONObject(jsonString);
 							Book book = new Book();
-							book.setId(worksJsonObject.optString("key").replace("/works/", ""));
-							book.setName(worksJsonObject.optString("title"));
+							book.setId(json.getString("key").replace("/works/", ""));
+							String bookTitle = json.getString("title");
+							book.setName(bookTitle);
+							JSONObject description = json.optJSONObject("description");
+							if(description!= null) book.setBookDesc(description.optString("value"));
 
-							JSONObject descriptionObj = worksJsonObject.optJSONObject("description");
-							if (descriptionObj != null)
-								book.setBookDesc(descriptionObj.optString("value"));
-
-							JSONObject publishedDateObj = worksJsonObject.optJSONObject("created");
-							if (publishedDateObj != null)
-								book.setPublishedDate(LocalDate.parse(publishedDateObj.optString("value")));
-
-							JSONArray coversJsonArray = worksJsonObject.optJSONArray("covers");
-							if (coversJsonArray != null) {
-								List<String> covers = new ArrayList<>();
-								for (int i = 0; i < coversJsonArray.length(); i++)
-									covers.add(coversJsonArray.getString(i));
-								book.setCoverIds(covers);
+							JSONArray coveArray = json.optJSONArray("covers");
+							if(coveArray!=null) {
+								List<String> coverIds = new ArrayList<String>();
+								for(int i=0;i<coveArray.length();i++){
+									coverIds.add(coveArray.getString(i));
+								}
+								book.setCoverIds(coverIds);
 							}
 
-							JSONArray authorsJsonArray = worksJsonObject.optJSONArray("authors");
-							if (coversJsonArray != null) {
+							JSONArray authorArray = json.optJSONArray("authors");
+							if(authorArray!=null){
 								List<String> authorIds = new ArrayList<>();
-								for (int i = 0; i < authorsJsonArray.length(); i++) {
-									String authorId = authorsJsonArray.getJSONObject(i)
-											.getJSONObject("author")
+								for(int i=0;i<authorArray.length();i++){
+									JSONObject authordIdObj = authorArray.getJSONObject(i);
+									authorIds.add(authordIdObj.getJSONObject("author")
 											.getString("key")
-											.replace("/authors/", "");
-									authorIds.add(authorId);
+											.replace("/authors/", ""));
+
 								}
 								book.setAuthorIds(authorIds);
-								List<String> authorNames = authorIds.stream().map(id -> authorRepository.findById(id))
-										.map(optionalAuthor -> {
-											if (optionalAuthor.isEmpty()) return "Unknown Author";
-											return optionalAuthor.get().getName();
+								List<String> authorNames = authorIds.stream().map(id->authorRepository.findById(id))
+										.map(optAuthor->{
+											if(!optAuthor.isPresent())return "Unknown Author";
+											return optAuthor.get().getName();
 										}).collect(Collectors.toList());
-
 								book.setAuthorNames(authorNames);
 							}
 
-						}
-						catch (Exception e){
-							e.printStackTrace();
-						}
+							JSONObject publishedJson = json.optJSONObject("created");
+							if(publishedJson!=null){
+								DateTimeFormatter dateFormat =DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+								String dateString = publishedJson.getString("value");
+								book.setPublishedDate(LocalDate.parse(dateString,dateFormat));
+							}
+							bookRepository.save(book);
+							System.out.println("Book saved:"+book.getName());
+
+						} catch (Exception e) {e.printStackTrace();}
 					}
 			);
-		}
-		catch ( Exception e){
+
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
 
+	}
 }
